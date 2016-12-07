d3232ed6f58066c62828676a55eb0e42bc6f109e
mvlabat <mvlabat@gmail.com>
2016-12-07 19:54:10 +0200

    Let RubyMine fix styling

--- diff_spec.rb
+++ diff_spec.rb
@@ -9,9 +9,9 @@ describe 'Diff implementation' do
     diff.load_from_file('file1.txt', 'file2.txt')
 
     expect(diff.send(:get_common_lines)).to eq({
-        2 => 1,
-        3 => 2,
-    })
+                                                   2 => 1,
+                                                   3 => 2,
+                                               })
   end
 
   it 'should get reversed common lines' do
@@ -31,13 +31,13 @@ describe 'Diff implementation' do
     diff.load_from_file('file1.txt', 'file2.txt')
 
     expect(diff.get_diff).to eq([
-        Diff::Line.new(Diff::MODIFIED, 'Some', 'Another'),
-        Diff::Line.new(Diff::REMOVED, 'Simple'),
-        Diff::Line.new(Diff::COMMON, 'Text'),
-        Diff::Line.new(Diff::COMMON, 'File'),
-        Diff::Line.new(Diff::ADDED, 'With'),
-        Diff::Line.new(Diff::ADDED, 'Additional'),
-        Diff::Line.new(Diff::ADDED, 'Lines'),
+                                    Diff::Line.new(Diff::MODIFIED, 'Some', 'Another'),
+                                    Diff::Line.new(Diff::REMOVED, 'Simple'),
+                                    Diff::Line.new(Diff::COMMON, 'Text'),
+                                    Diff::Line.new(Diff::COMMON, 'File'),
+                                    Diff::Line.new(Diff::ADDED, 'With'),
+                                    Diff::Line.new(Diff::ADDED, 'Additional'),
+                                    Diff::Line.new(Diff::ADDED, 'Lines'),
                                 ])
   end
 
